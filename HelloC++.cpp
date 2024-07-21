02446ef1513223660de7c9dec8980dfde1e94595
SPQR-0 <biomedical.tch.39@gmail.com>
2024-07-21 13:35:58 +0330

    Hello to the c++ world

---
+++ HelloC++.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // header file library
+using namespace std; """ It means we can use names for objects and variables from the standard library """
+
+""" main function code """
+int main() {
+    cout <<"Hello World";
+    return 0;  //return 0 = True, return 1 = False
+}
